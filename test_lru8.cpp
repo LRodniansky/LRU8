3470502563fc215e23b070018a2211e2a65dfa70
LRodniansky <4leonid@gmail.com>
2024-04-22 13:53:36 -0400

    Comments added.

--- test_lru8.cpp
+++ test_lru8.cpp
@@ -37,35 +37,36 @@ int main()
 	_data[5] = '5';
 	_data[6] = '6';
 	_data[7] = '7';
-	_data[8] = '8';
-	_data[9] = '9';
-	_data[10] = 'A';
+	_data[8] = '8';		// element 1 evicted.
+	_data[9] = '9';		// element 2 evicted.
+	_data[10] = 'A';	// element 3 evicted.
 
-	char l1 = _data[5];
-	char l2 = _data[10];
+	char l1 = _data[5];	// Value '5'.
+	char l2 = _data[10];	// Value 'A'.
 	
-	char &l3 = _data[11];
-	l3 = 'B';
+	char &l3 = _data[11];	// Reference to empty value. Element 4 evicted.
+	l3 = 'B';		// Now _data[11] == 'B'.
 
-	if (!_data.exists(1))
+	if (!_data.exists(1))	// Key 1 not exists because element 1 was previously evicted.
 		printf("_data[1] does not exist.\n");
 
-	char &l4 = _data[1];
-	l4 = 'C';
+	char &l4 = _data[1];	// Reference to empty value. Element 6 evicted.
+	l4 = 'C';		// Now data[1] == 'C'.
 
-	if (_data.exists(1))
+	if (_data.exists(1))	// Ley 1 exists. Element 1 just added.
 		printf("_data[1] exists.\n");
 
+	// _data[1] == 'C', _data[5] == '5', _data[10] == 'A', _data[11] == 'B'
 	printf("_data[1]=%c,_data[5]=%c, _data[10]=%c, data[11]=%c\n",_data[1], l1, l2,_data[11]);
 
 
 	LRDN::LRU8<std::string, int> _strings;
 
-	_strings["one"] = 1;
-	_strings["one"] = 2;
-	int n1 = _strings["one"];
+	_strings["one"] = 1;		// Element with key "one" is in LRU with value 1.
+	_strings["one"] = 2;		// Same element with key "one" is in LRU with value 2.
+	int n1 = _strings["one"];	// Get from LRU.
 
-	printf("_strings[\"one\"]=%d\n",n1);
+	printf("_strings[\"one\"]=%d\n",n1);	// n1 == 2
 
 	return 0;
 }
